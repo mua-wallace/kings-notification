e0e2ebc482534460c52172c18962d7c364615899
sinclaire <45803925+KambangSinclaire@users.noreply.github.com>
2022-05-16 22:56:22 +0000

    Update email.service.ts

--- src/modules/emails/email.service.ts
+++ src/modules/emails/email.service.ts
@@ -9,7 +9,7 @@ export class EmailService {
   async sendEmail(payload: INotification) {
 
     AWS.config.update({
-      region: process.env.AWS_REGION ?? "us-east-2",
+      region: process.env.AWS_REGION ?? "",
       accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? "",
     });
