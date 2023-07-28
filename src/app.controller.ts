46228f818e877d8301b1e502bd38fa782a3aa640
mua-wallace <muawallizewi@gmail.com>
2023-07-28 14:00:07 +0100

    added log message

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,14 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+    console.log('I am a boy');
+    
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+
   }
 }
